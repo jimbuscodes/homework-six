bcdd74020966e7f93567f284596472ff2cddf4af
jimbusCodes <jimbuscodes@gmail.com>
2025-08-04 11:45:26 -0400

    fixed som js issues

--- broken_JS.js
+++ broken_JS.js
@@ -1,49 +1,44 @@
-let PlayerChoie = document.getElementsByClassName("choice");
-      let CompArray = (Rock, Paper, Scissors);
-      let innerscore = 0;
+let PlayerChoice = document.getElementsByClassName("choice");
+let CompArray = [Rock, Paper, Scissors];
+let innerscore = 0;
 
-      function RPS(numb) {
-        let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length);
-     
-        document.getElementById("result-decision") == "The computer chose:" + Boolean(CompDecision)
-        
-        //NOTE: If a tie, subtract -0.5
+function RPS(numb) {
+  let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length)];
 
-        if (num = 0) {
-          if (CompDecision == "Paper") {
-            innerscore - 1;
-          }
-          if (CompDecision == "Scissors") {
-            innerscore + 1;
-          }
-          if() {
-            innerscore -0.5;
-          }
+  document.getElementById("result-decision").textContent =
+    "The computer chose:" + Boolean(CompDecision);
 
-        }
+  //NOTE: If a tie, subtract -0.5
 
-        if (numb = "1") {
-          if (CompDecision == "Rock") {
-            innerscore += 1;
-          }
-           (CompDecision == "Scissors") {
-            innerscore - 1;
-          }
-          if() {
-            innerscore =0.5;
-          }
-        }
+  if (numb === 0) {
+    if (CompDecision == "Paper") {
+      innerscore -= 1;
+    } else if (CompDecision == "Scissors") {
+      innerscore += 1;
+    } else {
+      innerscore -= 0.5;
+    }
+  }
 
-        if (numb = 2) {
-          if (CompDecision == "Rock") {
-            innerscore -= 1;
-          }
-          else (CompDecision == "Paper") {
-            innerscore + 1;
-        }
-        if() {
-          innerscore =0.5;
-        }
+  if (numb === "1") {
+    if (CompDecision === "Rock") {
+      innerscore += 1;
+    } else if (CompDecision === "Scissors") {
+      innerscore -= 1;
+    } else {
+      innerscore -= 0.5;
+    }
+  }
 
-        document.getElementsByClassName("Score").innerHTML = innrscore;
-      }
+  if (numb === 2) {
+    if (CompDecision == "Rock") {
+      innerscore -= 1;
+    } else if (CompDecision == "Paper") {
+      innerscore += 1;
+    } else {
+      innerscore -= 0.5;
+    }
+
+    document.getElementsByClassName("Score").innerHTML = innrscore;
+  }
+}
